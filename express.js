56601de0c4a696cc64e3bffc62bce60d59963270
Sandeep Sokle <sandeepsokle12@gmail.com>
2023-03-24 00:09:23 +0530

    update

--- express.js
+++ express.js
@@ -1,23 +1,14 @@
-
-
-
-
-
-
 let express = require("express");
 let data = require("./data.json");
 
-
-
 let server = express();
+server.use(cors())
 
-
-server.get("/data",(req,res)=>{
-    res.json(data);    
-})
-
+server.get("/data", (req, res) => {
+  res.json(data);
+});
 
 const port = process.env.PORT || 4000;
-server.listen(port, function(req, res) {
+server.listen(port, function (req, res) {
   console.log("Server has started at port 4000");
 });
